7b3182870d3cbd159b86b9e5daf191021e4d95e7
mi <649320373@qq.com>
2020-12-26 13:58:33 +0800

    update readme

---
+++ PrimeC++/Chapter1/if.cpp
@@ -0,0 +1,24 @@
+//if 
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+    int int_currVal,int_val;
+
+    int_currVal = 0;
+	int_val = 0;
+	
+	if (cin >> int_currVal)
+	{
+	int cnt = 1;
+	while(cin >> val)
+		{
+		if (int_val == currval)
+			++cnt;	
+		else {
+			cout << int_currVal << "			
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-#hello
-<at first> 
+hey
